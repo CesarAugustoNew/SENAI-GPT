321ac18ad4b4b1197ef514ee4dbe2edf156a3872
Cesar Augusto <cesarprofissional22@gmail.com>
2025-08-22 21:48:19 -0300

    js

---
+++ javascript/condicionais.js
@@ -0,0 +1,6 @@
+let idade = prompt(`Qual sua idade`);
+if (idade >= 18) {
+  alert(`Parabens! voce e maior de idade.`);
+} else {
+  alert(`Voce e menor de idade`);
+}
---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+console.log("wello world");
+
+let nome = prompt("Qual e seu nome ? ");
+let idade = prompt(`Qual a sua idade ? `);
+console.log(`Meu nome e ` + nome);
+alert(`meu nome e ${nome} e tenho ${idade} anos`);
+alert(`meu nome e ` + nome + `e minha idade e ` + idade);
